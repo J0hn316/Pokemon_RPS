4335aa17d632e4894b1842e7da3069ea90ebcd46
J0hn316 <jcordell316@gmail.com>
2023-08-07 16:08:35 -0400

    start

---
+++ ReadME.md
@@ -0,0 +1,1 @@
+## This is a Pokemon Based Rock Paper Scissors game.
---
+++ script.js
@@ -0,0 +1,9 @@
+const topMenu = document.querySelector("#top-menu");
+
+topMenu.style.height = "100%";
+
+let MenuLinks = [
+  { text: "Fire-types", href: "#", subLinks: [{ text: "Charmandar" }] },
+  { text: "Water-types", href: "#", subLinks: [{ text: "Squirtle" }] },
+  { text: "Grass-types", href: "#", subLinks: [{ text: "bulbasaur" }] },
+];

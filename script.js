3c527e02b030d14ab505cf9b32334c1684c5ce89
J0hn316 <jcordell316@gmail.com>
2024-05-23 19:03:36 -0400

    refactor JS code

--- script.js
+++ script.js
@@ -1,224 +1,108 @@
+const playerPokemonImg = document.querySelector('.PlayerPokemon');
+const computerPokemonImg = document.querySelector('.ComputerPokemon');
+const gameStart = document.querySelector('.container');
+const play = document.getElementById('play');
+const playAgain = document.getElementById('play-again');
+const winsEl = document.getElementById('Wins');
+const lossesEl = document.getElementById('Losses');
+const drawsEl = document.getElementById('Draws');
+const topMenu = document.querySelector('#top-menu');
+const subMenu = document.querySelector('#sub-menu');
+
+let topMenuLinks = document.querySelectorAll('a');
 let playerPokemon = null;
 let computerPokemon = null;
-
-const playerPokemonImg = document.querySelector(".PlayerPokemon");
-const computerPokemonImg = document.querySelector(".ComputerPokemon");
-const gameStart = document.querySelector(".container");
-const play = document.getElementById("play");
-const playAgain = document.getElementById("playagain");
-const winsEl = document.getElementById("Wins");
-const lossesEl = document.getElementById("Losses");
-const drawsEl = document.getElementById("Draws");
-
-const music = [
-  { name: "SBM", src: "audio/GLB.mp3" },
-  { name: "EBM", src: "audio/GLBV.mp3" },
-  { name: "PS", src: "audio/pokeball_sound_effects_mp3cut_1.mp3" },
-  { name: "lose", src: "audio/lose.wav" },
-];
-
 let song = new Audio();
-
-const pokemonList = [
-  { name: "Charizard", type: "fire", srcPath: "images/charizard.png" },
-  { name: "Blastoise", type: "water", srcPath: "images/blastoise.png" },
-  { name: "Venusaur", type: "grass", srcPath: "images/venusaur.png" },
-  { name: "Typhlosion", type: "fire", srcPath: "images/typhlosion.png" },
-  { name: "Feraligatr", type: "water", srcPath: "images/feraligatr.png" },
-  { name: "Meganium", type: "grass", srcPath: "images/meganium.png" },
-  { name: "Blaziken", type: "fire", srcPath: "images/blaziken.png" },
-  { name: "Swampert", type: "water", srcPath: "images/swampert.png" },
-  { name: "Sceptile", type: "grass", srcPath: "images/sceptile.png" },
-];
-
-const topMenu = document.querySelector("#top-menu");
-
-topMenu.style.height = "100%";
-
-topMenu.style.backgroundColor = "gray";
-
-let showingSubMenu = false;
-
-let menuLinks = [
-  {
-    text: "Fire-types",
-    href: "#",
-    subLinks: [
-      { text: "Charizard" },
-      { text: "Typhlosion" },
-      { text: "Blaziken" },
-    ],
-  },
-  {
-    text: "Water-types",
-    href: "#",
-    subLinks: [
-      { text: "Blastoise" },
-      { text: "Feraligatr" },
-      { text: "Swampert" },
-    ],
-  },
-  {
-    text: "Grass-types",
-    href: "#",
-    subLinks: [
-      { text: "Venusaur" },
-      { text: "Meganium" },
-      { text: "Sceptile" },
-    ],
-  },
-];
-
-menuLinks.forEach((el) => {
-  const link = document.createElement("a");
-  link.setAttribute("href", el.href);
-  link.innerText = el.text;
-  topMenu.appendChild(link);
-});
-
-const subMenu = document.querySelector("#sub-menu");
-
-subMenu.style.height = "100%";
-
-subMenu.style.backgroundColor = "Black";
-
-subMenu.style.position = "absolute";
-
-subMenu.style.top = "0";
-
-let topMenuLinks = document.querySelectorAll("a");
-
-topMenu.addEventListener("click", (el) => {
-  el.preventDefault();
-
-  let nextEl = menuLinks.filter((element) => {
-    return element.text === el.target.textContent;
-  });
-
-  for (let i = 0; i < topMenuLinks.length; i++) {
-    topMenuLinks[i].classList.remove("active");
-  }
-
-  if (el.target.tagName === "A") {
-    el.target.classList.add("active");
-
-    showingSubMenu = false;
-    subMenu.style.top = "0";
-  }
-
-  if (nextEl[0].subLinks) {
-    showingSubMenu = true;
-  }
-
-  if (showingSubMenu === true) {
-    let subLink = menuLinks.filter((element) => {
-      return element.text === el.target.textContent;
-    })[0].subLinks;
-
-    makeSubMenu(subLink);
-    subMenu.style.top = "100%";
-  } else {
-    showingSubMenu = false;
-    subMenu.style.top = "0";
-  }
-});
-
-function makeSubMenu(sublinks) {
-  subMenu.innerHTML = "";
-
-  for (let i = 0; i < sublinks.length; i++) {
-    let link = document.createElement("a");
-    link.href = sublinks[i].href;
-    link.textContent = sublinks[i].text;
-    subMenu.appendChild(link);
-  }
-}
-
-subMenu.addEventListener("click", (evt) => {
-  evt.preventDefault();
-
-  for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].name === evt.target.textContent) {
-      playerPokemon = pokemonList[i];
-      playerPokemonImg.src = pokemonList[i].srcPath;
-    }
-  }
-
-  const pcSelectedPokemon = Math.floor(Math.random() * pokemonList.length);
-  computerPokemon = pokemonList[pcSelectedPokemon];
-
-  computerPokemonImg.src = pokemonList[pcSelectedPokemon].srcPath;
-
-  playerPokemonImg.classList.remove("pokemonball1");
-  computerPokemonImg.classList.remove("pokemonball");
-
-  playSound("PS");
-
-  gameStart.classList.add("start");
-
-  setTimeout(() => {
-    gameStart.classList.remove("start");
-    battle();
-  }, 2500);
-
-  playAgain.style.visibility = "visible";
-
-  showingSubMenu = false;
-  subMenu.style.top = "0";
-});
 
 let wins = 0;
 let losses = 0;
 let draws = 0;
 
-function battle() {
-  if (computerPokemon.type === playerPokemon.type) {
-    alert("Its a tie");
-    console.log(`It's a tie!`);
-    draws++;
-    playMusic("");
-  } else if (
-    (playerPokemon.type === "fire" && computerPokemon.type === "grass") ||
-    (playerPokemon.type === "water" && computerPokemon.type === "fire") ||
-    (playerPokemon.type === "grass" && computerPokemon.type === "water")
-  ) {
-    alert("Winner");
-    playMusic("EBM");
-    console.log("Winner");
-    wins++;
-  } else {
-    alert("You lose");
-    playMusic("lose");
-    console.log("You Lose");
-    losses++;
-  }
-  winsEl.textContent = "Wins:" + " " + wins;
-  lossesEl.textContent = "Losses:" + " " + losses;
-  drawsEl.textContent = "Draws:" + " " + draws;
+const pokemonList = [
+  { name: 'Charizard', type: 'fire', srcPath: 'images/charizard.png' },
+  { name: 'Blastoise', type: 'water', srcPath: 'images/blastoise.png' },
+  { name: 'Venusaur', type: 'grass', srcPath: 'images/venusaur.png' },
+  { name: 'Typhlosion', type: 'fire', srcPath: 'images/typhlosion.png' },
+  { name: 'Feraligatr', type: 'water', srcPath: 'images/feraligatr.png' },
+  { name: 'Meganium', type: 'grass', srcPath: 'images/meganium.png' },
+  { name: 'Blaziken', type: 'fire', srcPath: 'images/blaziken.png' },
+  { name: 'Swampert', type: 'water', srcPath: 'images/swampert.png' },
+  { name: 'Sceptile', type: 'grass', srcPath: 'images/sceptile.png' },
+];
+
+let showingSubMenu = false;
+
+topMenu.style.height = '100%';
+topMenu.style.backgroundColor = 'gray';
+
+subMenu.style.height = '100%';
+subMenu.style.backgroundColor = 'Black';
+subMenu.style.position = 'absolute';
+subMenu.style.top = '0';
+
+const menuLinks = [
+  {
+    text: 'Fire-types',
+    href: '#',
+    subLinks: ['Charizard', 'Typhlosion', 'Blaziken'],
+  },
+  {
+    text: 'Water-types',
+    href: '#',
+    subLinks: ['Blastoise', 'Feraligatr', 'Swampert'],
+  },
+  {
+    text: 'Grass-types',
+    href: '#',
+    subLinks: ['Venusaur', 'Meganium', 'Sceptile'],
+  },
+];
+
+menuLinks.forEach((el) => {
+  const link = createLink(el.href, el.text);
+  topMenu.appendChild(link);
+});
+
+function createLink(href, text) {
+  const link = document.createElement('a');
+  link.setAttribute('href', href);
+  link.textContent = text;
+  return link;
 }
 
-play.addEventListener("click", () => {
-  playMusic("SBM");
-  playerPokemonImg.classList.add("pokemonball1");
-  computerPokemonImg.classList.add("pokemonball");
-  play.style.display = "none";
-});
+function updateSubMenu(subLinks) {
+  subMenu.innerHTML = '';
+  subLinks.forEach((subLink) => {
+    const link = createLink('#', subLink);
+    subMenu.appendChild(link);
+  });
+}
 
-playAgain.addEventListener("click", () => {
-  playAgain.style.visibility = "hidden";
-  location.reload();
-});
+function playSound(name) {
+  if (name === 'PS') {
+    let clip = new Audio('audio/pokeball_sound_effects_mp3cut_1.mp3');
+    clip.volume = 0.2;
+    clip.play();
+  }
+}
 
 function playMusic(name) {
-  if (name === "SBM") {
+  const music = [
+    { name: 'SBM', src: 'audio/GLB.mp3' },
+    { name: 'EBM', src: 'audio/GLBV.mp3' },
+    { name: 'PS', src: 'audio/pokeball_sound_effects_mp3cut_1.mp3' },
+    { name: 'lose', src: 'audio/lose.wav' },
+  ];
+
+  if (name === 'SBM') {
     song.src = music[0].src;
     song.volume = 0.3;
     song.play();
-  } else if (name === "EBM") {
+  } else if (name === 'EBM') {
     song.src = music[1].src;
     song.volume = 0.3;
     song.play();
-  } else if (name === "lose") {
+  } else if (name === 'lose') {
     song.src = music[3].src;
     song.volume = 0.3;
     song.play();
@@ -227,10 +111,98 @@ function playMusic(name) {
   }
 }
 
-function playSound(name) {
-  if (name === "PS") {
-    let clip = new Audio("audio/pokeball_sound_effects_mp3cut_1.mp3");
-    clip.volume = 0.2;
-    clip.play();
+function battle() {
+  let result = '';
+
+  if (playerPokemon.type === computerPokemon.type) {
+    result = "It's a tie";
+    draws++;
+  } else if (
+    (playerPokemon.type === 'fire' && computerPokemon.type === 'grass') ||
+    (playerPokemon.type === 'water' && computerPokemon.type === 'fire') ||
+    (playerPokemon.type === 'grass' && computerPokemon.type === 'water')
+  ) {
+    result = 'Winner';
+    wins++;
+    playMusic('EBM');
+  } else {
+    result = 'You lose';
+    losses++;
+    playMusic('lose');
   }
+  alert(result);
+  updateScore();
 }
+
+function updateScore() {
+  winsEl.textContent = `Wins: ${wins}`;
+  lossesEl.textContent = `Losses: ${losses}`;
+  drawsEl.textContent = `Draws: ${draws}`;
+}
+
+topMenu.addEventListener('click', (e) => {
+  e.preventDefault();
+  const target = e.target;
+
+  if (target.tagName === 'A') {
+    [...topMenu.querySelectorAll('a')].forEach((link) =>
+      link.classList.remove('active')
+    );
+
+    const selectedMenu = menuLinks.find(
+      (link) => link.text === target.textContent
+    );
+    if (selectedMenu?.subLinks) {
+      showingSubMenu = true;
+      updateSubMenu(selectedMenu.subLinks);
+      subMenu.style.top = '100%';
+    } else {
+      showingSubMenu = false;
+      subMenu.style.top = '0';
+    }
+  }
+});
+
+subMenu.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const selectedPokemon = pokemonList.find(
+    (pokemon) => pokemon.name === e.target.textContent
+  );
+
+  if (selectedPokemon) {
+    playerPokemon = selectedPokemon;
+    playerPokemonImg.src = playerPokemon.srcPath;
+
+    const randomIndex = Math.floor(Math.random() * pokemonList.length);
+    computerPokemon = pokemonList[randomIndex];
+    computerPokemonImg.src = computerPokemon.srcPath;
+
+    playerPokemonImg.classList.remove('pokemon-ball1');
+    computerPokemonImg.classList.remove('pokemon-ball');
+
+    playSound('PS');
+    gameStart.classList.add('start');
+
+    setTimeout(() => {
+      gameStart.classList.remove('start');
+      battle();
+    }, 2500);
+
+    playAgain.style.visibility = 'visible';
+    showingSubMenu = false;
+    subMenu.style.top = '0';
+  }
+});
+
+play.addEventListener('click', () => {
+  playMusic('SBM');
+  playerPokemonImg.classList.add('pokemon-ball1');
+  computerPokemonImg.classList.add('pokemon-ball');
+  play.style.display = 'none';
+});
+
+playAgain.addEventListener('click', () => {
+  playAgain.style.visibility = 'hidden';
+  location.reload();
+});
